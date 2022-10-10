de7fc742cf081e278783dce0f71ea78fa634959b
Klitrio <52793652+kallu717@users.noreply.github.com>
2022-10-10 09:24:26 +0400

    Add files via upload

---
+++ AdminDepartment.java
@@ -0,0 +1,16 @@
+public class AdminDepartment extends SuperDepartment {
+    String departmentName() {
+
+        return ("Admin Department");
+    }
+
+    String getTodaysWork() {
+
+        return ("Complete your documents Submission");
+    }
+
+    String getWorkDeadline() {
+
+        return ("Complete by EOD");
+    }
+}
